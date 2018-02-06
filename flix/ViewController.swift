caadbaa4858648acad2b7cf6a505dc34f631d40d
Sudarshan Prajapati <Sudarshan@sudarshans-macbook-pro.local>
2018-02-06 17:10:55 -0500

    Solved errors

--- flix/ViewController.swift
+++ flix/ViewController.swift
@@ -7,17 +7,19 @@
 //
 
 import UIKit
+import AlamofireImage
 
-class ViewController: UIViewController{
+class ViewController: UIViewController, UITableViewDataSource{
 
-    @IBOutlet weak var tableView: UITableView!
-   
+     @IBOutlet weak var tableView: UITableView!
+    
+    var movies :[[String:Any]] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
        
-        //tableView.dataSource = self
+        tableView.dataSource = self
         
         let url = URL(string: "https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed")!
         let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval : 10)
@@ -32,28 +34,42 @@ class ViewController: UIViewController{
             else if let data = data{
                 let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as! [String:Any]
                 let movies = dataDictionary["results"] as! [[String: Any]]
-                for movie in movies
-                {
-                    let title = movie["title"] as! String
-                    print(title)
-                }
+                self.movies = movies
+                self.tableView.reloadData()
                 
             }
         }
         task.resume()
     }
     
-        /*func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+        func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
         {
-            return 10
+            return movies.count
         }
         
         func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
         {
-            let cell = tableView.dequeueReusableCell(withIdentifier: "MovieCell", for: indexPath)
+            let cell = tableView.dequeueReusableCell(withIdentifier: "MovieCell", for: indexPath) as! MovieCell
+            
+            let movie = movies[indexPath.row]
+            
+            let title = movie["title"] as! String
+            let overview = movie["overview"] as! String
+            
+            let posterPathString = movie["poster_path"] as! String
+            
+            let baseUrlString = "https://image.tmdb.org/t/p/w500"
+            
+            let posterURL = URL(string: baseUrlString + posterPathString)!
+            
+            cell.posterImageView.af_setImage(withURL: posterURL)
+            
+            cell.titleLabel.text = title
+            cell.overviewLabel.text = overview
+            
             return cell
             
-        } */
+        }
         
         // Do any additional setup after loading the view, typically from a nib.
     
