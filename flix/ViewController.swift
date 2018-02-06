e9f969840aaa698fef750adf8515ee261bde17ab
Sudarshan Prajapati <Sudarshan@sudarshans-mbp.fios-router.home>
2018-02-06 11:27:30 -0500

    First Flix Commit

--- flix/ViewController.swift
+++ flix/ViewController.swift
@@ -8,12 +8,55 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController{
 
+    @IBOutlet weak var tableView: UITableView!
+   
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+       
+        //tableView.dataSource = self
+        
+        let url = URL(string: "https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed")!
+        let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval : 10)
+        let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
+        
+        let task = session.dataTask(with: request) { (data, response, error) in
+        //sdasdasd 
+        
+            if let error = error{
+                print(error.localizedDescription)
+            }
+            else if let data = data{
+                let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as! [String:Any]
+                let movies = dataDictionary["results"] as! [[String: Any]]
+                for movie in movies
+                {
+                    let title = movie["title"] as! String
+                    print(title)
+                }
+                
+            }
+        }
+        task.resume()
     }
+    
+        /*func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+        {
+            return 10
+        }
+        
+        func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
+        {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "MovieCell", for: indexPath)
+            return cell
+            
+        } */
+        
+        // Do any additional setup after loading the view, typically from a nib.
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
