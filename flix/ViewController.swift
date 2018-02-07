8e1b34955731d84d42ff072c2d2a4dd33952d015
Sudarshan Prajapati <Sudarshan@sudarshans-mbp.fios-router.home>
2018-02-06 23:15:40 -0500

    Final Commit

--- flix/ViewController.swift
+++ flix/ViewController.swift
@@ -13,21 +13,41 @@ class ViewController: UIViewController, UITableViewDataSource{
 
      @IBOutlet weak var tableView: UITableView!
     
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+   
     var movies :[[String:Any]] = []
+    var refreshcontrol: UIRefreshControl!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-       
+       refreshcontrol = UIRefreshControl()
+        refreshcontrol.addTarget(self, action: #selector(ViewController.didPullToRefresh(_:)), for: .valueChanged)
+        
+        tableView.insertSubview(refreshcontrol, at: 0)
         tableView.dataSource = self
         
+        activityIndicator.startAnimating()
+        
+        fetchmovies()
+        
+        
+            }
+    
+    func didPullToRefresh(_ refreshcontrol: UIRefreshControl )
+    {
+        fetchmovies()
+    }
+    
+    func fetchmovies()
+    {
         let url = URL(string: "https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed")!
         let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval : 10)
         let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
         
         let task = session.dataTask(with: request) { (data, response, error) in
-        //sdasdasd 
-        
+            //sdasdasd
+            
             if let error = error{
                 print(error.localizedDescription)
             }
@@ -35,11 +55,14 @@ class ViewController: UIViewController, UITableViewDataSource{
                 let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as! [String:Any]
                 let movies = dataDictionary["results"] as! [[String: Any]]
                 self.movies = movies
-                self.tableView.reloadData()
                 
+                self.activityIndicator.stopAnimating()
+                self.tableView.reloadData()
+                self.refreshcontrol.endRefreshing()
             }
         }
         task.resume()
+
     }
     
         func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
